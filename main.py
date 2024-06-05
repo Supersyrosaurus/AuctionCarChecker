931400943a243674a90e9281fca302ba4d3537ae
Supersyrosaurus <118223878+Supersyrosaurus@users.noreply.github.com>
2024-06-05 20:29:19 +0100

    undid MAJOR DAMAGE

--- main.py
+++ main.py
@@ -1,8 +1,69 @@
-import requests
 from bs4 import BeautifulSoup
-#URL = get('https://autotrader.co.uk')
-URL = requests.get('https://www.salvagemarket.co.uk')
-print(URL.status_code)
-#print(URL.content)
-soup = BeautifulSoup(URL.content, 'html5lib')
-print(soup.prettify())
+import requests
+URL1 = 'https://www.salvagemarket.co.uk/'
+URL2 = 'https://auctions.synetiq.co.uk/'
+URL3 = 'https://www.autotrader.co.uk/'
+page1 = 'https://auctions.synetiq.co.uk/auction/items/?make=0&fuel=0&transmission=0&category=5&layout=r50&seller=0&location=0&time=all&distance=&search=hatchback&sort=2&tab=0'
+print(''.join(list(page1)))
+
+r = requests.get(page1)
+soup = BeautifulSoup(r.content, 'html5lib')
+#print(soup.prettify())
+
+
+table = soup.findAll('div', attrs = {'class' : 'row vehicle_list'})
+
+#intable = table.find('h2', attrs = {'class' : 'ml-2 mr-2'})
+
+#print(table)
+
+#################     FOR SYNETIQ CAR AUCTIONS WEBSITE      ######################
+
+def chngePg(lstPg, link):
+    split = list(link)
+    lsEnd = len(split) - 1
+    nxtPg = int(split[lsEnd]) + 1
+    if nxtPg > lstPg:
+        return'END'
+    else:
+        split[lsEnd] = str(nxtPg)
+        newLnk = ''.join(split)
+        return newLnk
+     
+    
+
+def lstPgSynetiq():
+    lstPgSection = soup.find('section', attrs = {'class' : 'section-alt'})
+    lstPgls = lstPgSection.findAll('li', attrs = {'class' : 'page-item'})
+    lstPgNum = len(lstPgls) - 1
+    lstPg = lstPgls[lstPgNum].find('a', attrs = {'class' : 'page-link'}).get_text()
+    return(int(lstPg))
+
+def searchSynetiqPage():
+    cars = []
+    for row in table:
+
+        car = {}
+        car['Name'] = row.find('h2').get_text()
+        car['Price'] = row.find('span', attrs = {'class' : 'list_price_2'}).get_text()
+
+        category = row.find('i', attrs = {'class' : 'fa fa-tag fa-fw list-fa_2 fa-mr10'})
+        catPrnt = category.parent.parent #had to go up 2 steps to find parent of this
+        car['Category'] = catPrnt.find('span', attrs = {'class' : 'list-group-text-item'}).get_text()
+        
+        mileage = row.find('i', attrs = {'class' : 'fa fa-tachometer-alt fa-fw list-fa_2 fa-mr10'})
+        milPrnt = mileage.parent.parent 
+        car['Mileage'] = milPrnt.find('span', attrs = {'class' : 'list-group-text-item'}).get_text()
+        
+        cars.append(car)
+    return cars
+
+def searchSalvageMarket():
+    None
+
+def searchCopart():
+    None
+
+
+print(chngePg(14, 'https://auctions.synetiq.co.uk/auction/items/?x=0&type=1&tab=1&list=1&section_id=&make=&transmission=0&fuel=0&seller=&category=&location=0&sort=&time=0&distance=0&options=&layout=r20&search=&fp=0&page=2'))
+    
